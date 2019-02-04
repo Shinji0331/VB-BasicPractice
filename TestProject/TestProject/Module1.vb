39fe2773fa4e3efd4f08a1e0226d7c8639eacb57
Shinji0331 <f.e.company0331@gmail.com>
2019-02-04 12:45:34 +0900

    1

---
+++ TestProject/TestProject/Module1.vb
@@ -0,0 +1,9 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Visual Basicは楽しいよ")
+        Console.ReadLine()
+
+    End Sub
+
+End Module
---
+++ sample1/sample1/Module1.vb
@@ -0,0 +1,9 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("よくわかるVisual Basic")
+        Console.ReadLine()
+
+    End Sub
+
+End Module
